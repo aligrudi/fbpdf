7aff443fa2e735b80c79f1a2b4f4e26c656a7902
Ali Gholami Rudi <ali@rudi.ir>
2009-08-28 13:53:30 +0430

    add copyright notice

--- fbpdf.c
+++ fbpdf.c
@@ -1,3 +1,13 @@
+/*
+ * fbpdf - A small framebuffer pdf viewer using poppler
+ *
+ * Copyright (C) 2009 Ali Gholami Rudi
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License, as published by the
+ * Free Software Foundation.
+ *
+ */
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
