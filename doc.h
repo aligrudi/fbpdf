a64e68806211414598507dbf884fb4670bf68086
Ali Gholami Rudi <ali@rudi.ir>
2013-03-14 18:06:56 +0330

    doc: use the generic FB_VAL() by default
    I commited my local FB_VAL() for 32-bit RGB framebuffers
    by mistake.

--- doc.h
+++ doc.h
@@ -2,8 +2,7 @@
 typedef unsigned int fbval_t;
 
 /* optimized version of fb_val() */
-//#define FB_VAL(r, g, b)	fb_val((r), (g), (b))
-#define FB_VAL(r, g, b)        (((r) << 16) | ((g) << 8) | (b))
+#define FB_VAL(r, g, b)	fb_val((r), (g), (b))
 
 struct doc *doc_open(char *path);
 int doc_pages(struct doc *doc);
